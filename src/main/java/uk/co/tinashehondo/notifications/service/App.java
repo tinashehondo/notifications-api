729bf5a9de42b69d44e6565deee08bcf4e588f85
tinashehondo <tinashehondo@gmail.com>
2020-05-28 15:38:34 +0200

    Initial commit to show openapi example

---
+++ src/main/java/uk/co/tinashehondo/notifications/service/App.java
@@ -0,0 +1,18 @@
+package uk.co.tinashehondo.notifications.service;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ApplicationContext;
+import uk.co.tinashehondo.notifications.rest.api.NotificationApi;
+
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class,
+        scanBasePackages = { "uk.co.tinashehondo.notifications.rest.api"})
+public class App {
+    public static void main(String[] args) {
+        ApplicationContext context = new SpringApplicationBuilder(App.class)
+                .registerShutdownHook(true).run(args);
+        NotificationApi plansApiController = context.getBean(NotificationApi.class);
+        System.out.println(">>>>>>>>>>>>>>>>>>>>>>>>>>>> {}"+plansApiController);
+    }
+}
